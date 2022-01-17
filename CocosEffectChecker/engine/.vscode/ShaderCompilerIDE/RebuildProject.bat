18fd61370a88b78cb062055c70d0d9e822b777fb
jk20012001 <jkregister@163.com>
2022-01-17 20:00:10 +0800

    Hide Cmd Window

--- CocosEffectChecker/engine/.vscode/ShaderCompilerIDE/RebuildProject.bat
+++ CocosEffectChecker/engine/.vscode/ShaderCompilerIDE/RebuildProject.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0ShaderCompilerIDE\Tools\
-copy /y ..\ShaderCompilerIDE_template.vcxproj ..\ShaderCompilerIDE.vcxproj
-copy /y ..\ShaderCompilerIDE_template.vcxproj.filters ..\ShaderCompilerIDE.vcxproj.filters
+copy /y ..\ShaderCompilerIDE_template.vcxproj ..\ShaderCompilerIDE.vcxproj >nul >nul
+copy /y ..\ShaderCompilerIDE_template.vcxproj.filters ..\ShaderCompilerIDE.vcxproj.filters >nul >nul
 dllfunc goldfx.dll CocosShaderCompilerIDEGenerator "%~dp0ShaderCompilerIDE\ShaderCompilerIDE.vcxproj" "%~dp0..\..\"
 @echo --------------------------------------------------------------------------------------------------------------------
 @echo 请在Visual Studio中的工具菜单--选项--文本编辑器--文件扩展名中分别添加effect和chunk为Microsoft Visual C++类型的编辑器
