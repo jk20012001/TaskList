cd7f158ad9bfd62bce1cdc97af0b5f74b1d9ce15
jk20012001 <jkregister@163.com>
2022-03-19 13:03:33 +0800

    update new tasks and keybinding

--- CocosEffectChecker/CopyFiles.bat
+++ CocosEffectChecker/CopyFiles.bat
@@ -14,7 +14,10 @@ copy /y ..\ShaderCompilerIDE.vcxproj ..\ShaderCompilerIDE_template.vcxproj
 copy /y ..\ShaderCompilerIDE.vcxproj.filters ..\ShaderCompilerIDE_template.vcxproj.filters
 dllfunc tool.dll CocosShaderCompilerIDEGenerator "%1\.vscode\ShaderCompilerIDE\ShaderCompilerIDE\ShaderCompilerIDE.vcxproj" "%1"
 
-dllfunc tool.dll toolVSCodeAddKeyBinding "ctrl+f7 keyincomposition" "workbench.action.tasks.build"
+dllfunc tool.dll toolVSCodeAddKeyBinding "ctrl+f7 keyincomposition" "-workbench.action.tasks.build"
+dllfunc tool.dll toolVSCodeAddKeyBinding_RunTask "ctrl+f7" "Cocos Effect Checker"
+dllfunc tool.dll toolVSCodeAddKeyBinding_RunTask "ctrl+alt+f7" "Cocos Effect Checker With Performance"
+dllfunc tool.dll toolVSCodeAddKeyBinding_RunTask "ctrl+shift+f7" "Cocos Effect Editor"
 dllfunc tool.dll toolVSCodeAddKeyBinding "ctrl+shift+b" "-workbench.action.tasks.build"
 dllfunc tool.dll toolVSCodeAddKeyBinding "ctrl+shift+b" "workbench.action.tasks.runTask"
 
