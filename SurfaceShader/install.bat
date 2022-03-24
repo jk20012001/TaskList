f2e9ba70344a44eaa1bd9415ea02d4943f1683c1
jk20012001 <jkregister@163.com>
2022-03-24 22:52:53 +0800

    add VSOutput_clipPos for terrain

---
+++ SurfaceShader/install.bat
@@ -0,0 +1,17 @@
+@echo off
+if "%1"=="" (
+@echo 请拖engine文件夹到bat上
+pause
+exit
+)
+
+cd /d "%1"
+cd editor\assets\chunks
+mklink /J common "%~dp0chunks\common\"
+mklink /J lighting-models "%~dp0chunks\lighting-models\"
+mklink /J post-process "%~dp0chunks\post-process\"
+mklink /J shading-entries "%~dp0chunks\shading-entries\"
+mklink /J surfaces "%~dp0chunks\surfaces\"
+cd ..\effects
+mklink /J surfaces "%~dp0effects\surfaces\"
+pause
