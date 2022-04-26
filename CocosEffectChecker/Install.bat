b6379bcfd128738f2a81024915e2df3374be5722
jk20012001 <jkregister@163.com>
2022-04-26 12:09:14 +0800

    fix va extension support

--- CocosEffectChecker/Install.bat
+++ CocosEffectChecker/Install.bat
@@ -25,11 +25,14 @@ color 04
 pause
 )
 
+If not exist "C:\ZRM2000\" (
+reg import "%~dp0\engine\.vscode\ShaderCompilerIDE\ShaderCompilerIDE\Tools\VA_HLSLExt.reg"
+)
+
 cls
 color 02
 @echo --------------------------------------------------------------------------------------------------------------------
 @echo 请在Visual Studio中的工具菜单--选项--文本编辑器--文件扩展名中分别添加effect和chunk为Microsoft Visual C++类型的编辑器
 @echo 同时勾选将无扩展名映射为Microsoft Visual C++类型, 然后重启Visual Studio以生效
-@echo VA设置中的Projects and Files勾选Parse files without extensions as headers
 @echo --------------------------------------------------------------------------------------------------------------------
 pause
