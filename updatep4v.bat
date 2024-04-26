b6f007042c484c5577d567d11916b5c53721e351
eugenejin <eugenejin@tencent.com>
2024-04-26 18:37:36 +0800

    updatep4v

--- updatep4v.bat
+++ updatep4v.bat
@@ -13,8 +13,10 @@ for /f %%i in (c:\templog\envvar.txt)  Do set %%i
 
 set workspace=%pathname%
 if "%workspace%" == "master" set workspace=master_ssd
-echo workspace is %workspace%
-pause
+c:\goldapps\consoletools echocolor ffff00 "workspace is %workspace%"
+@echo=
+set /p bClean=需要Clean所有已修改内容吗?(y/n):
+
 
 SET PERFORCE="C:\Program Files\Perforce\p4.exe"
 %PERFORCE% set P4USER=eugenejin
@@ -40,7 +42,17 @@ rem clean必须cd到指定路径内再执行才可以, 并且不能带路径参数
 rem sync最好是指定路径, 它不会管当前路径, 不指定路径的话会将整个workspace全部更新一遍
 if not exist %1 goto :EOF
 cd %1
-%PERFORCE% clean -e
+if "%bClean%"=="y" %PERFORCE% clean -e
+if exist bin\ACMobileClient.uproject copy /y bin\ACMobileClient.uproject .
+if not exist bin\path.ini copy %~dp0path.ini bin\
 cd /d %~dp0
 %PERFORCE% sync %1...#head
+goto :EOF
+
+
+:Log
+if not exist %1 goto :EOF
+cd %1
+%PERFORCE% clean -l
+cd /d %~dp0
 goto :EOF
