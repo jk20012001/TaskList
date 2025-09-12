530164d3a913f74987211cc8e931bcc4db1f893a
jk20012001 <jkregister@163.com>
2025-09-12 11:03:22 +0800

    Starp

--- 元梦快捷操作.bat
+++ 元梦快捷操作.bat
@@ -3,13 +3,15 @@
 @echo lua:			Push LUA脚本以调试元梦手机包, 需要先复制需要推送的文件列表到剪贴板(非文件名)
 @echo luapc:			Push LUA脚本以调试元梦PC端包, 需要先复制需要推送的文件列表到剪贴板(非文件名)
 @echo lua3:			修改并Push三个固定LUA脚本到手机以运行本地Cook的包
+@echo resetgame:		游戏在前台时直接重启游戏
 @echo cmdline:		选择并Push UE4CommandLine到安卓手机, Insight和renderdoc都需要
 @echo shipping:		修改本地代码以便配合Shipping包资源
 @echo renderdoc:		修改本地代码以便安卓打包抓帧, 启动com.tencent.letsgo/com.epicgames.ue4.GameActivityExt
 @echo console:		发送控制台命令到安卓手机, 分号分隔
 @echo runui:			选择Trace的CommandLine并执行UnrealInsight
 @echo ios:			修改IOS的DefaultEngine, 需要将ini拷到%~dp0下, 拷完无需重新Generate工程, 直接编译即可
-@echo initandroid:		添加安卓打包所需的环境变量
+@echo android:		安卓打小包相应的工程设置, 需要将工程文件夹拖到bat上
+@echo initandroid:		添加VS编译生成安卓apk所需的环境变量
 @echo dumplog:		dump安卓log
 @echo editor:			启动编辑器和工程, 需要将工程文件夹拖到bat上
 @echo runcook:		Push安卓本地cook的资源和三个固定LUA脚本到手机, 并启动游戏, 需要将工程文件夹拖到bat上
@@ -39,6 +41,7 @@ if "%choice%"=="cmdline"	call %EXEC% UEMobilePushCommandLine 0 %PackageName% %Pr
 if "%choice%"=="console"	call %EXEC% UESendConsoleString 0 & pause & exit  rem r.MeshDrawCommands.UseCachedCommands 0
 if "%choice%"=="runui"		call %EXEC% UEMobilePushCommandLine 0 %PackageName% %ProjectName% & call %EXEC% UEMobileExecUnrealInsight & pause & exit
 if "%choice%"=="ios"		call %EXEC% UEModifyDefaultEngineIOSRuntime %~dp0DefaultEngine.ini & pause & exit
+if "%choice%"=="resetgame"	call %EXEC% toolAndroidResetForegroundApp & pause & exit
 if "%choice%"=="initandroid"	(
 	%EXEC% toolSetUserEnvironmentValue AGDE_JAVA_HOME "C:\Program Files\Java\jdk-20\"
 	%EXEC% toolSetUserEnvironmentValue ANDROID_HOME C:\Users\eugenejin\AppData\Local\Android\Sdk
@@ -77,6 +80,7 @@ if not exist "%PROJECTDIR%" (
 echo %PROJECTDIR% >"%RECORDFILE%"
 call %CONSOLETOOLS% echocolor ff0000ff "当前选择的项目文件夹为%PROJECTDIR%"
 
+if "%choice%"=="android"	call %EXEC% StarPAndroidLittlePackageSettings "%PROJECTDIR%" & pause & exit
 if "%choice%"=="shipping"	call %EXEC% UEMobileModifyCodeForShippingPak "%PROJECTDIR%" & echo 修改完成, 需要重新编译工程 & pause & exit
 if "%choice%"=="renderdoc"	call %EXEC% UEMobileModifyCodeForRenderDoc "%PROJECTDIR%" & echo 修改完成, 需要重新编译工程 & pause & exit
 if "%choice%"=="xlspath"	(
